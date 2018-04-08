7f0b77a289e32b06635131a62fa17c3b19f0a43d
Maja Graonic <m.graonic@gmail.com>
2018-04-07 23:15:54 -0700

    all tests passing

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,24 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
+
+# integer division truncates decimals:
+# num /= 10 removes a digit
+# num *= 10 adds a digit
+
+# modulus returns remainder:
+# num % 10 accesses last digit
+
 def is_palindrome(number)
-  raise NotImplementedError
+  return false if number == nil || number < 0
+
+  num = number
+  reverse = 0
+
+  while num > 0
+    last = num % 10
+    reverse = (reverse * 10) + last
+    num /= 10
+  end
+
+  reverse == number ? true : false
+
 end
